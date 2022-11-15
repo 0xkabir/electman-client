dc421cb0bdc2504fe2c7a37f4c16e92ae4cf0c2b
0xkabir <kabir.sadiken1@gmail.com>
2022-11-15 12:42:55 +0600

    added functionality to delete user review

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import ReviewCard from './ReviewCard';
 
@@ -12,11 +13,24 @@ const MyReviews = () => {
             setReviews(data)
         })
     },[user])
+    const deleteReview = _id => {
+        fetch(`http://localhost:5000/reviews/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data.deletedCount){
+                toast.success('deleted successfully')
+                const remaining = reviews.filter(review => review._id !== _id)
+                setReviews(remaining)
+            }
+        })
+    }
     return (
         <div className='w-11/12 md:w-4/5 mx-auto min-h-screen'>
             <h2 className='text-4xl font-medium text-center my-5'>My Reviews</h2>
             <div className='grid md:grid-cols-2 lg:grid-cols-3'>
-            {reviews.map(serviceReview=><ReviewCard key={serviceReview._id} serviceReview={serviceReview}></ReviewCard>)}
+            {reviews.map(serviceReview=><ReviewCard key={serviceReview._id} serviceReview={serviceReview} deleteReview={deleteReview}></ReviewCard>)}
         </div>
         </div>
     );
--- src/pages/MyReviews/ReviewCard.js
+++ src/pages/MyReviews/ReviewCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ReviewCard = ({serviceReview}) => {
+const ReviewCard = ({serviceReview, deleteReview}) => {
     const {serviceName, review} = serviceReview
     return (
         <div className='p-3 m-3.5 text-center rounded-lg hover:shadow-md'>
@@ -8,7 +8,7 @@ const ReviewCard = ({serviceReview}) => {
             <p>{review}</p>
             <div className='text-center w-3/4 mx-auto font-medium'>
                 <button className='border px-4 py-1 my-1 mx-2.5 border-emerald-600 rounded text-emerald-600 hover:text-white hover:bg-emerald-600'>Update</button>
-                <button className='border px-4 py-1 my-1 mx-2.5 border-orange-600 rounded text-orange-600 hover:text-white hover:bg-orange-600'>Delete</button>
+                <button className='border px-4 py-1 my-1 mx-2.5 border-orange-600 rounded text-orange-600 hover:text-white hover:bg-orange-600' onClick={()=>deleteReview(serviceReview._id)}>Delete</button>
             </div>
         </div>
     );
