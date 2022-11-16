a24fad057d22da4caea74d81f162aba1f6389379
0xkabir <kabir.sadiken1@gmail.com>
2022-11-16 12:35:36 +0600

    updated add service page

--- src/pages/AddService/AddService.js
+++ src/pages/AddService/AddService.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 import useTitle from '../../hooks/useTitle'
 
 const AddService = () => {
@@ -7,11 +8,12 @@ const AddService = () => {
         event.preventDefault()
         const form = event.target;
         const name = form.name.value;
-        const imgurl = form.imgurl.value;
+        const imgurl = form.thumbimgurl.value;
+        const fullimg = form.fullimgurl.value;
         const price = form.price.value;
         const intro = form.intro.value;
         const description = form.description.value;
-        const servcieObj = {name, imgurl, price, intro, description}
+        const servcieObj = {name, imgurl, fullimg, price, intro, description}
         fetch('https://electman-server.vercel.app/services', {
             method: "POST",
             headers: {
@@ -20,14 +22,20 @@ const AddService = () => {
             body: JSON.stringify(servcieObj)
         })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            if(data.acknowledged){
+                form.reset()
+                toast.success('Service added successfully')
+            }
+        })
     }
     return (
         <div className='text-center'>
             <h2 className='text-5xl font-medium text-center my-10'>Add a Service</h2>
             <form onSubmit={handleAddService}>
                 <input type="text" name="name" placeholder='Service Name' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
-                <input type="text" name="imgurl" placeholder='Image URL' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
+                <input type="text" name="thumbimgurl" placeholder='Thumbnail Image URL' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
+                <input type="text" name="fullimgurl" placeholder='Full Size Image URL' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
                 <input type="text" name="price" placeholder='Price (in $)' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
                 <textarea type="text" name="intro" rows="2" placeholder='Introductory Text' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
                 <textarea type="text" name="description" rows="5" placeholder='Description' className='block w-11/12 md:w-3/4 lg:w-1/2 mx-auto px-2.5 py-1 border-2 border-gray-400 my-2.5 rounded focus:border-orange-600 ' required/>
